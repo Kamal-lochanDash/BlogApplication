daced3e169d8a32fd890f95b2c41aebc8344dcb8
Kamal-lochanDash <kamallochandash04@gmail.com>
2024-12-22 23:14:04 +0530

    added blogs to the dataBase

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,12 +1,25 @@
+const Blog=require("../models/blog");
+
+
 async function handelgetAddBlog(req,res) {
     return res.render("Addblogs");
 }
 
 
 async function handelPostAddBlog(req,res) {
+    console.log(req?.user?._id)
     console.log(req?.body)
     console.log(req?.file)
-    return res.end("meow");
+
+    const{title,description,content}=req?.body;
+   const blog= await Blog.create({
+        title:title,
+        description:description,
+        content:content,
+      createdBy:req?.user?._id,
+      coverImageURL:`/uploads/${req?.file?.filename}`
+    });
+    return res.redirect("/")
 }
 
 module.exports={
